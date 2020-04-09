74aefb7a22af93e9a11a2d0944b787cfadfe2382
Somrat <58439442+Somrat25@users.noreply.github.com>
2020-04-09 20:41:12 +0600

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,46 @@
+ magians=['sam','alice','david']
+ for magian in magians:
+	print(magian.title()+",You are great! We want you on our day")
+	print("come soon!"+magian.title()+".\n")
+	print("Thank you everyone")
+  
+  
+  	
+Sam,You are great! We want you on our day
+co
+Thank you everyone
+Alice,You are great! We want you on our day
+come soon!Alice.
+
+Thank you everyone
+David,You are great! We want you on our day
+come soon!David.
+
+Thank you everyoneme soon!Sam.
+  
+  
+  
+  
+ pizzas=['chicken','mutton','beef']
+ for pizza in pizzas:
+	print(pizza)
+	print("I hate "+pizza)
+	print("Too much hate !"+pizza+" Drag me to hell"+".\n")
+	print("I hate pizza too much")
+
+	
+chicken
+I hate chicken
+Too much hate !chicken Drag me to hell.
+
+I hate pizza too much
+mutton
+I hate mutton
+Too much hate !mutton Drag me to hell.
+
+I hate pizza too much
+beef
+I hate beef
+Too much hate !beef Drag me to hell.
+
+I hate pizza too much
