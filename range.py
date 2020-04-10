d77cf75eb0ae101956c2f6e2a34561db1039f26e
Somrat <58439442+Somrat25@users.noreply.github.com>
2020-04-10 19:17:25 +0600

    Add files via upload

---
+++ range.py
@@ -0,0 +1,46 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Numerical list
+>>> #range() function
+>>> for number in range(1,8)
+>>> for number in range(1,8):
+	print(number)
+
+	
+
+>>> #list() number using range()
+>>> #list(range())
+>>> pieof#
+>>> 
+>>> #list[range()]
+>>> numbers = list[range(1,6)]
+>>> #list(range())
+>>> values = list(range(1,6))
+>>> print(values)
+[1, 2, 3, 4, 5]
+>>> even_numbers=list(range2,21,2)) #skip numbers
+>>> even_numbers = list(range(2,21,2)) #skip numbers as it add 2 per executions
+>>> print(numbers)
+>>> even_numbers=list(range(2,31,2))
+>>> print(even_numbers)
+[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+>>> 
+>>> square = []
+>>> for value in range(1,21)
+>>> square =[]
+>>> for value in range(1,41):
+	square=value**2
+	square.append(square)
+	print(square)
+
+	
+
+>>> square=[]
+>>> for value in range(1,31):
+	square=value**2
+	square.append(squares)
+	print(square)
+
+	
+
+>>> 
