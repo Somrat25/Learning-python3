849ef18548fc1970b2686f23b94411c7c3335099
Somrat <58439442+Somrat25@users.noreply.github.com>
2020-04-17 20:39:12 +0600

    Add files via upload

---
+++ datetime.py
@@ -0,0 +1,23 @@
+'''import datetime
+
+x = datetime.datetime.now()
+print(x)'''
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.strftime("%A"))
+print(x.strftime("%B"))
+print(x.strftime("%a"))
+print(x.strftime("%b"))
+print(x.strftime("%w"))
+print(x.strftime("%d"))
+print(x.strftime("%m"))
+print(x.strftime("%y"))
+print(x.strftime("%Y"))
+print(x.strftime("%h"))
+print(x.strftime("%I"))
+print(x.strftime("%W"))
+print(x.strftime("%c"))
